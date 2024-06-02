8d65bc4af211bdecdfba4422e6daea4fac4fb55b
Iman sanjaya <imansanjaya800@gmail.com>
2024-06-02 11:10:09 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ekraf',
+  webDir: 'www'
+};
+
+export default config;
